de11d02ef87720f798a9cecc8ff65c8c2a923cf3
Adrian Karuna Soetikno <royankaruna@gmail.com>
2025-02-06 00:34:02 +0700

    test thermer

--- app/api/print/route.ts
+++ app/api/print/route.ts
@@ -1,13 +1,15 @@
 export async function GET() {
-  const printData = [
-    {
-      type: 0,
-      content: 'Hello World!',
-      bold: 1,
-      align: 1,
-      format: 0
-    }
-  ];
+  const printData = {
+    data: [
+      {
+        type: 0,
+        content: 'Hello World!',
+        bold: 1,
+        align: 1,
+        format: 0
+      }
+    ]
+  };
 
   return new Response(JSON.stringify(printData), {
     headers: {
