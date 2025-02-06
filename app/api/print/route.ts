594e7240b482a9ae35085816dd32d3e7876127be
Adrian Karuna Soetikno <royankaruna@gmail.com>
2025-02-06 12:40:44 +0700

    test 3 line

--- app/api/print/route.ts
+++ app/api/print/route.ts
@@ -1,17 +1,11 @@
 export async function GET() {
   const receiptData = {
     "0": {
-      type: 4,
-      content: `
-        <div style="width: 100%; text-align: center; font-size: 24px;">
-          <br><br>
-          <h1 style="font-size: 36px;">TADSHEEN STORE</h1>
-          <p>Item: Product</p>
-          <p>Price: $10.00</p>
-          <p>Thank You!</p>
-          <br><br>
-        </div>
-      `
+      type: 0,
+      content: "This text has<br />three<br />lines",
+      bold: 0,
+      align: 0,
+      format: 4
     }
   };
 
